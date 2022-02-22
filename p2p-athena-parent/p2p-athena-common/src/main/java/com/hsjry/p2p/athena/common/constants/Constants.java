216035aa82ed2e199312008eb646bee78495dd1d
dtwsemail <dtwsemail@users.noreply.github.com>
2022-02-22 23:20:08 +0800

    Delete p2p-athena-parent directory

--- p2p-athena-parent/p2p-athena-common/src/main/java/com/hsjry/p2p/athena/common/constants/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.hsjry.p2p.athena.common.constants;
-
-/**
- * Created by wangyf14377 on 2018/4/9.
- */
-public class Constants {
-
-    public final static String EMPTY_STRING = "";
-}
--- p2p-athena-parent/p2p-athena-dal/src/main/java/com/hsjry/p2p/athena/dal/integration/mtbank/protocol/socket/request/component/XmlResponseBean.java
+++
@@ -1,8 +0,0 @@
-package com.hsjry.p2p.athena.dal.integration.mtbank.protocol.socket.request.component;
-
-
-/**
- * Created by wangyf14377 on 2018/4/14.
- */
-public class XmlResponseBean {
-}
